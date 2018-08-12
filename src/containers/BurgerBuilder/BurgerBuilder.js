cb6b1cf6121e7853ffd0a941a79614ae08939cd6
shivam123425 <shivam123425@gmail.com>
2018-08-12 21:36:57 +0530

    Hard code BurgerIngredients in Burger, add Burger in BurgerBuilder

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
+
 import Aux from "../../hoc/Aux";
 
 class BurgerBuilder extends Component {
@@ -6,7 +8,7 @@ class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        <Burger />
         <div>Build Controls</div>
       </Aux>
     );
