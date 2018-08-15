0c1d88a907df2765369b6b930a8d3af20f14fde5
shivam123425 <shivam123425@gmail.com>
2018-08-15 18:26:00 +0530

    Add SideDrawer with Logo and NavigationItems

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 import classes from "./Layout.css";
 
 import Aux from "../../hoc/Aux";
 const layout = props => (
   <Aux>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
