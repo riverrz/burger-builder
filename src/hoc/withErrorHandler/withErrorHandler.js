c2a87c0522d178e4faaf04f067f591fa86774a7b
shivam123425 <shivam123425@gmail.com>
2018-08-18 20:23:48 +0530

    Remove axios interceptors on unmount

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -8,13 +8,13 @@ const withErrorHandler = (WrappedComponent, axios) => {
       error: null
     };
     componentWillMount() {
-      axios.interceptors.request.use(request => {
+      this.reqInterceptor = axios.interceptors.request.use(request => {
         this.setState({
           error: null
         });
         return request;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         res => res,
         error => {
           this.setState({
@@ -23,6 +23,10 @@ const withErrorHandler = (WrappedComponent, axios) => {
           return Promise.reject(error);
         }
       );
+    }
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.response.eject(this.resInterceptor);
     }
     errorConformedHandler = () => {
       this.setState({
