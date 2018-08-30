a5634f043b5e80231f6c3cd2889d7524c71e5a1f
shivam123425 <shivam123425@gmail.com>
2018-08-30 23:49:44 +0530

    Fix order button active state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -12,7 +12,6 @@ import * as actionTypes from "../../store/actions";
 
 class BurgerBuilder extends Component {
   state = {
-    purchaseable: false,
     purchasing: false,
     loading: false,
     error: false
@@ -42,9 +41,7 @@ class BurgerBuilder extends Component {
       .reduce((total, el) => {
         return total + el;
       }, 0);
-    this.setState({
-      purchaseable: sum > 0 ? true : false
-    });
+    return sum > 0;
   };
 
   purchaseHandler = () => {
@@ -101,7 +98,7 @@ class BurgerBuilder extends Component {
             disabled={disabledInfo}
             ingredientsRemoved={this.props.onIngredientRemoved}
             totalPrice={this.props.price}
-            purchaseable={this.state.purchaseable}
+            purchaseable={this.updatePurchaseState()}
             ordered={this.purchaseHandler}
           />
         </Aux>
