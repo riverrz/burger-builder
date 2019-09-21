a8e701b01ec53bc5ed107804a6f646c70ac45818
shivam123425 <shivam123425@gmail.com>
2019-09-21 23:51:50 +0530

    Create auth reducer

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -8,10 +8,11 @@ export const authStart = () => {
   };
 };
 
-export const authSuccess = authData => {
+export const authSuccess = (token, userId) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
-    authData
+    idToken: token,
+    userId
   };
 };
 
@@ -39,7 +40,7 @@ export const auth = (email, password, isSignup) => {
       .post(url, authData)
       .then(response => {
         console.log(response);
-        dispatch(authSuccess(response.data));
+        dispatch(authSuccess(response.data.idToken, response.data.localId));
       })
       .catch(err => {
         console.log(err);
